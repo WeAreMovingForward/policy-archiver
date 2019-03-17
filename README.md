dd43da54acb47022646dee80c03badb7beedcce7
gshahbazian <gabeshahbazian@gmail.com>
2019-03-17 16:34:35 -0700

    Load policies from airtable

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 [Work in progress](https://github.com/gshahbazian/movingforward-archiver/projects/1)
 
 [Example output](https://www.dropbox.com/sh/m3i4zci3cwnve6z/AAB0V3x7xGh1vVE-jL7Aej6Ia?dl=0)
+
+## Running
+
+Create a `.env` with your airtable api key:
+```
+echo 'AIRTABLE_API_KEY=_api_key_here_' > .env
+```
+
+Alternatively, you can run with an exported csv of the `#MovingForward Orgs` airtable:
+```
+npm start ~/#MovingForward.csv
+```
+
+The archived files are exported to the `./out` directory, with pdfs and plaintext files for each policy.
--- index.js
+++ index.js
@@ -1,38 +1,73 @@
+require('dotenv').config()
 const fs = require('fs-extra')
 const csvparse = require('csv-parse/lib/sync')
+const airtable = require('airtable')
 const PolicyLoader = require('./policy-loader')
 const HTMLPolicyLoader = require('./html-policy-loader')
 
-const inputFilePath = process.argv.slice(2)
-if (!inputFilePath.length) {
-  console.log(`
-    missing path to csv download
-    ex: npm start ~/#MovingForward.csv
-  `)
-  process.exit()
+function getCSVPolicies (filePath) {
+  const policies = []
+  const csv = fs.readFileSync(filePath, 'utf8')
+  csvparse(csv, {
+    columns: true,
+    skip_empty_lines: true
+  }).forEach(record => {
+    const name = record[`﻿Organization`] // GABE: clean up this unicode char
+    const url = record['Policy Link']
+    if (!name || !url) { return }
+    policies.push({ name, url })
+  })
+  return policies
 }
 
-const csv = fs.readFileSync(inputFilePath[0], 'utf8')
-
-const records = csvparse(csv, {
-  columns: true,
-  skip_empty_lines: true
-})
-
-const outPath = './out/'
-fs.removeSync(outPath)
-fs.mkdirSync(outPath)
+async function getAirtablePolicies () {
+  return new Promise((resolve, reject) => {
+    const policies = []
+    const base = airtable.base('appIdDbM8UmDkocYT')
+    base('#MovingForward Orgs').select({
+      fields: ['Organization', 'Policy Link'],
+      view: 'Grid view -- All Details'
+    }).eachPage((records, fetchNextPage) => {
+      records.forEach((record) => {
+        const name = record.get('Organization')
+        const url = record.get('Policy Link')
+        if (!name || !url) { return }
+        policies.push({ name, url })
+      })
+      fetchNextPage()
+    }, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(policies)
+      }
+    })
+  })
+}
 
 async function run () {
+  let policies = []
+  const inputFilePath = process.argv.slice(2)
+  if (inputFilePath.length) {
+    policies = getCSVPolicies(inputFilePath[0])
+  } else if (process.env.AIRTABLE_API_KEY) {
+    policies = await getAirtablePolicies()
+  }
+
+  if (!policies.length) {
+    console.log(`Error loading policies, please check README for usage.`)
+    process.exit()
+  }
+
+  // Clean out directory
+  const outPath = './out/'
+  fs.removeSync(outPath)
+  fs.mkdirSync(outPath)
+
   await HTMLPolicyLoader.start()
 
-  for (let record of records) {
-    const name = record[`﻿Organization`] // GABE: clean up this unicode char
-    const policyURL = record['Policy Link']
-
-    if (!name || !policyURL) { continue }
-
-    const loader = new PolicyLoader(name, policyURL)
+  for (let policy of policies) {
+    const loader = new PolicyLoader(policy.name, policy.url)
     await loader.process()
   }
 
